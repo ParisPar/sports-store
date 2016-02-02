c81ee58c386f7f9a7877a711656c2797a10a224f
paris <parispar@users.noreply.github.com>
2016-02-02 23:52:38 +0200

    Finish order placement functionality

--- controllers/sportsStore.js
+++ controllers/sportsStore.js
@@ -1,36 +1,63 @@
-var app = angular.module('sportsStore');
+angular.module('sportsStore')
+  .constant('productsUrl', 'https://sports-store-1912.firebaseio.com/products')
+  .constant('ordersUrl', 'https://sports-store-1912.firebaseio.com/orders')
+  .controller('sportsStoreCtrl', function($scope, $timeout, $firebaseArray, $location, productsUrl, ordersUrl, cart) {
 
-app.controller('sportsStoreCtrl', function($scope, $timeout, $firebaseArray) {
+    var myProductsRef = new Firebase(productsUrl);
+    var myOrdersRef = new Firebase(ordersUrl);
+    
+    //Using angularFire
+    $scope.data = {
+      products: $firebaseArray(myProductsRef)
+    };
 
-  var myProductsRef = new Firebase("https://sports-store-1912.firebaseio.com/products");
-  
-  //Using angularFire
-  $scope.data = {
-    products: $firebaseArray(myProductsRef)
-  };
+    //Without using angularFire
+    /*
+    $scope.data = {};
+    $scope.data.products = [];
 
-  //Without using angularFire
-  /*
-  $scope.data = {};
-  $scope.data.products = [];
+    myProductsRef.on('value', function(snapshot) {
 
-  myProductsRef.on('value', function(snapshot) {
+      //For more info on using the $timeout service:
+      //http://mancdev.tumblr.com/post/104675276404/using-timeout-in-angular-to-safely-refresh
+      //http://stackoverflow.com/questions/22154578/data-from-firebase-not-loading-on-route-change-but-does-on-refresh
+      $timeout(function() {
+        var productsObject = snapshot.val();
 
-    //For more info on using the $timeout service:
-    //http://mancdev.tumblr.com/post/104675276404/using-timeout-in-angular-to-safely-refresh
-    //http://stackoverflow.com/questions/22154578/data-from-firebase-not-loading-on-route-change-but-does-on-refresh
-    $timeout(function() {
-      var productsObject = snapshot.val();
+        for(obj in productsObject)
+          $scope.data.products.push(productsObject[obj]);
+      });
 
-      for(obj in productsObject)
-        $scope.data.products.push(productsObject[obj]);
+    }, function(errorObject) {
+      console.log("The read failed: " + errorObject.code);
     });
+    */
+ 
+    $scope.sendOrder = function(shippingDetails) {
 
-  }, function(errorObject) {
-    console.log("The read failed: " + errorObject.code);
+      //Use deep copy to be able to manipulate the data without affecting
+      //the other parts of the application
+      var order = angular.copy(shippingDetails);
+      order.products = cart.getProducts();
+
+      var orders = $firebaseArray(myOrdersRef);
+
+      orders.$add(order)
+
+        .then(function(myOrdersRef) {
+          $scope.data.orderId = myOrdersRef.key();
+          $scope.data.orderError = null;
+        })
+
+        .catch(function(error){
+          $scope.data.orderError = error;
+          console.log("Error: ", error);
+        })
+
+        .finally(function(){
+          $location.path("/complete");
+        });
+
+    }
+
   });
-  */
-  
-
-
-});
